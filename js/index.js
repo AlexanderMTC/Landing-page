9e96b2e24cc612e8665d07ee43d9fb5f5f8d5b65
Alexander Troncoso <alexander.mtc@gmail.com>
2023-06-12 15:58:34 -0300

    Add slider en galeria

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@
     menuLink = document.querySelectorAll(".menu__link"),
     scrollButton = document.querySelector(".scrollTop"),
     lightbox = document.querySelectorAll(".gallery__img"),
-    lightClose = document.querySelector(".gallery__lightbox--close");
+    lightClose = document.querySelector(".gallery__lightbox--close"),
+    sliders = document.querySelectorAll(".gallery__img"),
+    buttonNext = document.querySelector(".gallery__rightArrow"),
+    buttonBefore = document.querySelector(".gallery__leftArrow");
 
-  // Menu //
+  let value;
+
+  // MENU //
 
   menuLink.forEach((link) => {
     link.addEventListener("click", () => {
@@ -13,7 +18,7 @@
     });
   });
 
-  // Scroll //
+  // SCROLL //
 
   scrollButton.addEventListener("click", (e) => {
     window.scrollTo({
@@ -32,17 +37,49 @@
     }
   });
 
-  //Lightbox //
+  // LIGHTBOX //
+  //Lightbox--Slider //
+
+  buttonNext.addEventListener("click", () => {
+    changeProsition(1);
+  });
+
+  buttonBefore.addEventListener("click", () => {
+    changeProsition(-1);
+  });
+
+  const changeProsition = (add) => {
+    currentImg = document.querySelector(".gallery__img--show").dataset.id;
+    value = Number(currentImg);
+    value += add;
+
+    if (value === sliders.length + 1 || value === 0) {
+      value = value === 0 ? sliders.length : 1;
+    }
+
+    document.querySelector(".gallery__img--show").src =
+      sliders[value - 1].getAttribute("src");
+    document.querySelector(".gallery__img--show").alt =
+      sliders[value - 1].getAttribute("alt");
+    document.querySelector(".gallery__img--show").dataset.id =
+      sliders[value - 1].getAttribute("data-id");
+  };
+
+  //Lightbox--Show //
 
   lightbox.forEach((imagen) => {
     imagen.addEventListener("click", () => {
-      document.querySelector(".gallery__lightbox").style.display = "block";
+      document.querySelector(".gallery__lightbox").style.display = "grid";
       document.querySelector(".gallery__img--show").src =
         imagen.getAttribute("src");
       document.querySelector(".gallery__img--show").alt =
         imagen.getAttribute("alt");
+      document.querySelector(".gallery__img--show").dataset.id =
+        imagen.getAttribute("data-id");
     });
   });
+
+  //Lightbox--Close //
 
   lightClose.addEventListener("click", () => {
     document.querySelector(".gallery__lightbox").style.display = "none";
